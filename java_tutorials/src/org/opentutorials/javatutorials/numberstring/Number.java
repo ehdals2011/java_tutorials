7248da0fe3a0b86b8c1b23158960b59b21186b88
unknown <1393028@hansung.edu>
2019-08-18 20:15:47 +0900

    java_tutorials

---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/Number.java
@@ -0,0 +1,8 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(4+2);
+	}
+}
---
+++ java_tutorials/src/org/opentutorials/javatutorials/variable/Demo.java
@@ -0,0 +1,22 @@
+package org.opentutorials.javatutorials.variable;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		double a;
+		a= 1.1;
+		
+		System.out.println(a+1.1);
+		*/
+		//String first;
+		//first = "coding";
+		//String first = "coding";
+		String a, b;
+		a= "coding";
+		b= "everybody";
+		System.out.println(a+b);
+	}
+
+}
